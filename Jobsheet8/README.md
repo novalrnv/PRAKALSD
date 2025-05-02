e6731dcc1e985f392de8ce483d064c32024593b1
novalrnv <akuunoval@gmail.com>
2025-05-02 14:48:28 +0700

    Percobaan1

---
+++ Jobsheet8/README.md
@@ -0,0 +1,10 @@
+|  | Algorithm and Data Structure |
+|--|--|
+| NIM |  244107020210 |
+| Nama |  KHOIRUL UMAM NOVALIDI |
+| Kelas | TI - 1H |
+| Repository | [link](https://github.com/novalrnv/PRAKALSD.git) |
+
+# STACK
+
+## 8.1 
---
+++ Jobsheet8/sc_code/Mahasiswa12.java
@@ -0,0 +1,15 @@
+public class Mahasiswa12 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa12(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
